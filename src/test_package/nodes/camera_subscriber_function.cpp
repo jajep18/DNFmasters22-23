38b9ff7345fcfe88db0be71de617e9d1304fb875
jajep18 <jajep18@student.sdu.dk>
2022-10-25 14:35:24 +0200

    Created new world and added circle detection

--- src/test_package/nodes/camera_subscriber_function.cpp
+++ src/test_package/nodes/camera_subscriber_function.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/opencv.hpp>
 #include "rclcpp/logging.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include<vector>
 
 using std::placeholders::_1; // MIght not be necessary
 
@@ -55,16 +56,60 @@ using std::placeholders::_1; // MIght not be necessary
 
 // };
 
+// void circle_pub_callback(std::vector<cv::Vec3f> circles){
+//   for (size_t i = 0; i < circles.size(); i++)
+//   {
+//     std::cout << "Circle [x, y, r] " << i <<" data: ";
+//     for (size_t j = 0; j < circles[i].rows ; j++)
+//     {
+//       std::cout << circles[i][j];
+//     }
+//     std::cout << "." <<std::endl;
+//   }
+  
+// }
+
 void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg) //Image::ConstSharedPtr & msg //Image::SharedPtr  msg
 {
+  cv::Mat src = cv_bridge::toCvShare(msg, "bgr8")->image;
   try {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(10);
+    cv::imshow("view", src);
+    // cv::waitKey(10);
   } catch (const cv_bridge::Exception & e) {
     auto logger = rclcpp::get_logger("my_subscriber");
     RCLCPP_ERROR(logger, "Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
+
+  // Hough Transform
+
+    cv::Mat gray;
+    cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+    cv::medianBlur(gray, gray, 5);
+    std::vector<cv::Vec3f> circles;
+    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
+                 gray.rows/16,  // change this value to detect circles with different distances to each other
+                 100, 30, 1, 30 // change the last two parameters
+            // (min_radius & max_radius) to detect larger circles
+    );
+    //Draw center and outline of all detected circles 
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        cv::Vec3i c = circles[i];
+        cv::Point center = cv::Point(c[0], c[1]);
+        // circle center
+        cv::circle( src, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
+        // circle outline
+        int radius = c[2];
+        cv::circle( src, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
+    }
+  //Show detected circles
+  circle_pub_callback(circles);
+  cv::imshow("view", src);
+
+
 }
+
+
 
 int main(int argc, char * argv[])
 {
@@ -79,15 +124,13 @@ int main(int argc, char * argv[])
     
     // Image transport
     image_transport::ImageTransport it(node);//it(nh);
-    image_transport::Subscriber sub = it.subscribe("diff_drive_robot/custom_rgb/image_raw", 1, imageCallback);
-    // /diff_drive_robot/custom_rgb/image_raw //camera/image
+    image_transport::Subscriber sub = it.subscribe("top_down_cam/custom_rgb/image_raw", 1, imageCallback);
 
-    //rclcpp::spin(std::make_shared<camSubscriber>());
+    //Start node
     rclcpp::spin(node);
 
-    //Close window again
+    //Close windows again
     cv::destroyWindow("view");
-
     rclcpp::shutdown();
     return 0;
 }
