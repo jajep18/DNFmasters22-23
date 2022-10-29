b2a19d9713b8c6ccdf038cde35bc3f07b590d049
jajep18 <jajep18@student.sdu.dk>
2022-10-29 12:46:35 +0200

    Found better solution for circle information

--- src/test_package/nodes/camera_pubsub_function.cpp
+++ src/test_package/nodes/camera_pubsub_function.cpp
@@ -77,45 +77,53 @@ private:
     std::string circleStr;
     void timer_callback(){
         auto message = std_msgs::msg::String();
-        if (!circles.empty())
-            circleStr = vec3fToString(circles);
+        // if (!circles.empty())
+        //     circleStr = vec3fToString(circles);
         stringToVec3f(circleStr);
-        message.data = "Detected " + std::to_string(circles.size() ) + " circles!" +"dividerCounter = "+ std::to_string(dividerCounter) + testStr +" \n" + circleStr;
+        message.data = "Detected " + std::to_string(circles.size() ) + " circles!";
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
         publisher_->publish(message);
     }
 
-    std::string vec3fToString(std::vector<cv::Vec3f> vec){
-        std::string vecStr;
-        for (size_t i = 0; i < vec.size(); i++)
-        {
-            vecStr.append(std::to_string(vec[i][0])+','+std::to_string(vec[i][1])+','+std::to_string(vec[i][2])+";");
-        }
-        return vecStr;
-    }
+    // std::string vec3fToString(std::vector<cv::Vec3f> vec){
+    //     std::string vecStr;
+    //     for (size_t i = 0; i < vec.size(); i++)
+    //     {
+    //         vecStr.append(std::to_string(vec[i][0])+','+std::to_string(vec[i][1])+','+std::to_string(vec[i][2])+";");
+    //     }
+    //     return vecStr;
+    // }
 
-    int dividerCounter = 0; //Delete this later!
-    std::string testStr;
+    // int dividerCounter = 0; //Delete this later!
+    // std::string testStr;
 
-    //std::vector<cv::Vec3f> 
-    void stringToVec3f(std::string str){
-        dividerCounter = 0;
-
-        // cv::Vec3f 3fvec;
-        size_t pos = 0;  
-        std::string tempString; // define a string variable  
+    // //std::vector<cv::Vec3f> 
+    // void stringToVec3f(std::string str){
+    //     dividerCounter = 0;
+    //     std::vector<cv::Vec3f> vec;
+    //     cv::Vec3f vec3f;
+    //     size_t pos = 0;  
+    //     std::string tempString1, tempString2; // define string temps  
         
-        // use find() function to get the position of the delimiters  
-        while ( ( pos = str.find(";") ) != std::string::npos)  
-        {  
-            tempString = str.substr(0, pos); // store the substring   
-            str.erase(0, pos + 1);  /* erase() function store the current positon and move to next token. */  
-            dividerCounter++; 
-            testStr.append(tempString);
-        }  
+    //     // use find() function to get the position of the delimiters  
+    //     while ( ( pos = str.find(";") ) != std::string::npos)  
+    //     {  
+    //         tempString1 = str.substr(0, pos); // store the substring   
+    //         str.erase(0, pos + 1);  /* erase() function store the current positon and move to next token. */  
+    //         dividerCounter++; 
+    //         // testStr.append(tempString1);
+    //     }  
 
-        // return 3fvec;
-    }
+    //     while ( ( pos = tempString1.find(",") ) != std::string::npos)  
+    //     {  
+    //         tempString2 = tempString1.substr(0, pos); // store the substring   
+    //         tempString1.erase(0, pos + 1);  /* erase() function store the current positon and move to next token. */  
+    //         vec3f.append(std::stof(tempString2));
+    //         // tempString2.append(tempString1);
+    //     }  
+
+    //     // return 3fvec;
+    // }
 };
 
 
