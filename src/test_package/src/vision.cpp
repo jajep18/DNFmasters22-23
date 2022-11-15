336e44224dc65683edda721303442fb9ae739dce
jajep18 <jajep18@student.sdu.dk>
2022-11-15 11:43:38 +0100

    Started work on hue software

--- src/test_package/src/vision.cpp
+++ src/test_package/src/vision.cpp
@@ -6,6 +6,29 @@
 
 using std::placeholders::_1; // MIght not be necessary
 
+
+float averageHueFromCircle(cv::Mat image, cv::Vec3f circle){
+  cv::Mat cvtImage;
+  image.copyTo(cvtImage);
+  cv::cvtColor(image,cvtImage, cv::COLOR_BGR2HSV); //converting BGR to HSV 
+
+  cv::Mat roi = cvtImage(cv::Range(circle[1]-circle[2], circle[1]+circle[2]), cv::Range(circle[0]-circle[2], circle[0]+circle[2]));
+   // ROI from minX & maxX, minY & maxY. x coordinate - radius = minX   x coordinate + radius = maxX
+  
+  //Calculate mean color of ROI
+  float temp = 0;
+  float insideCounter = 0;
+  for (size_t row = 0; row < image.rows; row++){
+    for (size_t col = 0; col < image.cols; col++){
+      if( ( sqrt( pow(row-circle[0],2) + pow(col-circle[1],2) ) <= circle[2] ) ) {
+        temp += image.at<cv::Vec3b>(row, col)[0]; //* roi.at<cv::Vec3b>(row, col)[1];
+        insideCounter++;
+      }
+    } 
+  }
+  return temp/insideCounter;
+}
+
 std::vector<cv::Vec6f> detect_circles(cv::Mat &image){
 
     std::vector<cv::Vec3f> circles;
@@ -30,6 +53,7 @@ std::vector<cv::Vec6f> detect_circles(cv::Mat &image){
         
         //Circle color
         cv::Vec3b color = image.at<cv::Vec3b>(center);
+        float avgHue = averageHueFromCircle(image, c);
 
         // circle center
         cv::circle( image, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
@@ -37,15 +61,24 @@ std::vector<cv::Vec6f> detect_circles(cv::Mat &image){
         // circle outline
         int radius = c[2];
         cv::circle( image, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
-
-        // Create output vector //Crasher nok her !!!
+        
+        // Create output vector 
         circleInfoTemp[0] = c[0];
         circleInfoTemp[1] = c[1];
-        circleInfoTemp[2] = c[2];
+        // circleInfoTemp[2] = c[2];
+        circleInfoTemp[2] = avgHue;
         circleInfoTemp[3] = color[0];
         circleInfoTemp[4] = color[1];
         circleInfoTemp[5] = color[2];
         circleInfo.push_back(circleInfoTemp);
+
+        cv::putText(image, //target image
+              std::to_string(i),    // Text
+              center,
+              cv::FONT_HERSHEY_DUPLEX,
+              0.6,
+              CV_RGB(225, 255, 255),  // Font color
+              2);
     }
   cv::putText(image, //target image
                 "Detected " + std::to_string(circles.size() ) + " circles" ,    // Text
@@ -56,7 +89,9 @@ std::vector<cv::Vec6f> detect_circles(cv::Mat &image){
                 2);
 
   //Return detected circles
+  // cv::cvtColor(image,image, cv::COLOR_RGB2HSV);
   return circleInfo;
 
 }
+
 
