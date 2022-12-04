1c840b96f2c8bf26a8ce49028f56773aa24c131f
jajep18 <jajep18@student.sdu.dk>
2022-12-04 13:25:15 +0100

    RCLCPP does not work, reeee
    Co-authored-by: Erik Lindby <ELindby@users.noreply.github.com>

--- src/dnf_package/nodes/dnf_pubsub_function.cpp
+++ src/dnf_package/nodes/dnf_pubsub_function.cpp
@@ -3,7 +3,8 @@
 #include "std_msgs/msg/string.hpp"
 #include "rclcpp/logging.hpp" //For "get_logger"
 #include <iostream>
-#include "../src/dnf_lib.cpp"
+//#include "../src/dnf_lib.cpp"
+#include <torch/torch.h>//
 
 // Custom messages
 #include "custom_msgs/msg/circle_info.hpp"
@@ -17,7 +18,7 @@ public:
   DNFNode() : Node("dnf_pubsub"){
     subscription_ = this->create_subscription<custom_msgs::msg::CircleInfoArr>(
       "cam_circle_topic", 1, std::bind(&DNFNode::topic_callback, this, _1));
-    DNFinit();
+    //DNFinit();
   }
 
 private:
