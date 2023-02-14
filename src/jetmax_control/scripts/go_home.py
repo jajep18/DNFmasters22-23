6971c8918794b0f55883f4805fcfc7d85f1411f8
ELindby <erik.lindby@gmail.com>
2023-02-14 13:21:56 +0100

    Updated comments and gohome

--- src/jetmax_control/scripts/go_home.py
+++ src/jetmax_control/scripts/go_home.py
@@ -3,7 +3,9 @@ import rclpy
 from jetmax_control.srv import FK, FKRequest, FKResponse
 
 if __name__ == "__main__":
-    rclpy.init_node("go_home")
+    rclpy.init()
+    node = rclpy.rclpy.create_node("go_home")
+    node.get_logger().info('Created node')
     go_home = rclpy.ServiceProxy("/jetmax_control/forward_kinematics", FK) #Node der kalder en service. Skal laves om til ROS2
     ret = go_home(FKRequest(90, 90, 0))
     rclpy.logdebug(ret)
